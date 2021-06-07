0fec85b14b4adecad99bcb0fa8dbe2682508d9f6
kevin55100 <85477585+kevin55100@users.noreply.github.com>
2021-06-07 16:32:42 +0200

    Update App.java

--- Java1/src/tridenombre/App.java
+++ Java1/src/tridenombre/App.java
@@ -5,7 +5,6 @@ import java.util.Scanner; // importer le composant Scanner
 public class App 
 {
     // code du composant
-
     public static void main(String[] args) {
         
         int a;
@@ -15,21 +14,48 @@ public class App
 
         System.out.println("Systeme de tri des nombre dans l'ordre croissant");
         System.out.println("Veuillez saisir les nombre : ");
-
+        
         // vos algorithme
 
         sc = new Scanner(System.in);
+
+        System.out.printf("a = ");
         a = sc.nextInt();
+        System.out.printf("b = ");
         b = sc.nextInt();
+        System.out.printf("c = ");
         c = sc.nextInt();
 
+        System.out.println("l'ordre croissant est : ");
+        
         sc.close(); // obligatoire (libère les ressources)
 
-        if(a<=b && a<=c && b<=c); 
+        if(a<=b && a<=c && b<=c) 
         {
-            System.out.format("mettre dans l'ordre" +a,b,c);
+            System.out.println("a,b,c");
+        }
+        else if(a<=b && a<=c && c<=b)
+        {
+            System.out.println("a,c,b");
         }
 
+        if(b<=a && b<=c && a<=c)
+        {
+            System.out.println("b,a,c");
+        }    
+        else if(b<=a && b<=c && c<=a)
+        {
+            System.out.println("b,c,a");
+        }
+
+        if(c<=a && c<=b && a<=b)
+        {
+            System.out.println("c,a,b");
+        }
+        else if(c<=a && c<=b && b<=a)
+        {
+            System.out.println("c,b,a");
+        }
 
 
     }
