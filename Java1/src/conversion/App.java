5c021c249e43799c55fb6fc596cab7551a1911f2
kevin <kbourguignon@hotmail.com>
2021-06-10 12:42:27 +0200

    Update App.java

--- Java1/src/conversion/App.java
+++ Java1/src/conversion/App.java
@@ -7,34 +7,46 @@ public class App
 {
     // code du composant
     public static void main(String[] args) {
-
+      
         double kilometre;
         double miles;
         Scanner sc;
-        
+        String q = "";
         
         System.out.println("Convertir les Kilomètre en Miles");
+        
+        sc = new Scanner(System.in);
+        
+        System.out.println("Saisissez une chaine : ");
+         sc.nextLine(); 
+        System.out.println(" Appuyer sur une touche pour quitter ");
+        
+       
+        
+        
+        
 
 
         // vos Algoritme
+       
         
 
-        sc = new Scanner(System.in);
-       
-
-
-       
-
         do {
-            System.out.println("Veuillez rentrer le kilométrage : ");
+            System.out.println("Veuillez rentrer le kilométrage : "); 
             kilometre = sc.nextDouble();
         } 
         while (kilometre < 0.01 || kilometre > 1000000);
        
-        miles = kilometre / 1.609;
-        System.out.printf("%.2f Miles", miles);
+        miles = kilometre*1/ 1.609;
+        System.out.printf("%.2f kilomètre = %.2f Miles", kilometre ,miles);
+        
+        
+        
+        
 
-        if(kilometre/1.609 == 0)
+        
+        
+        
         
 
         sc.close(); // obligatoire (libère les ressource)
