3256a7af7b32bf442ddf4dfb945c0f8c837b98fa
kevin55100 <85477585+kevin55100@users.noreply.github.com>
2021-06-10 17:33:51 +0200

    Update App.java

--- Java1/src/conversion/App.java
+++ Java1/src/conversion/App.java
@@ -2,46 +2,62 @@ package conversion;
 
 import java.util.Scanner; //importer les composant du Scanner
 
+
 public class App
 
 {
     // code du composant
     public static void main(String[] args) {
+
       
+
         double kilometre;
         double miles;
         Scanner sc;
         String saisie;
-        String [] chaine; 
+         
         
-        System.out.println("Convertir les Kilomètre en Miles");
-        
-        sc = new Scanner(System.in);
-        saisie = sc.nextLine(); // peut recevoir q ou un nombre
-                  
-        
-        // vos Algoritme
+        System.out.println("Conversion des kilomètre <-> miles"); 
+
+       sc = new Scanner(System.in);
        
+       
+       
+
         
 
-        do {
+        // vos Algoritme
+       
+         
+        do {           
             System.out.println("Veuillez rentrer le kilométrage : "); 
             kilometre = sc.nextDouble();
         } 
         while (kilometre < 0.01 || kilometre > 1000000);
-       
+
+        saisie = sc.nextLine(); // peut recevoir q ou un nombre
         miles = kilometre*1/ 1.609;
         System.out.printf("%.2f kilomètre = %.2f Miles", kilometre ,miles);
-        
-        if(saisie = q);
+         
+                
+        if(saisie == "q") 
         {
-            System.out.println("fin du programme");
-            System.exit(0);
-
+            System.exit(0); 
         }
+       
+         
+            
         
+            
+            
+
+          
         
+
         
+
+        
+       
 
         
         
