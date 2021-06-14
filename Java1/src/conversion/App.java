1c1dfe4cb51107da718b8665871e1fd9aa8e40e2
kevin <kbourguignon@hotmail.com>
2021-06-14 16:59:27 +0200

    Update App.java

--- Java1/src/conversion/App.java
+++ Java1/src/conversion/App.java
@@ -15,7 +15,7 @@ public class App
         double miles;
         Scanner sc;
         String saisie;
-         
+        String [] chaine;
         
         System.out.println("Conversion des kilomètre <-> miles"); 
 
@@ -24,27 +24,33 @@ public class App
        
         // vos Algoritme
        
-         
+         saisie = sc.nextLine(); // peut recevoir q ou un nombre
+
+
         do {           
             System.out.println("Veuillez rentrer le kilométrage : ");  
             kilometre = sc.nextDouble();
-        } 
-        while (kilometre < 0.01 || kilometre > 1000000); // fera le calcul si les kilometres et entre les deux proposition
-
-        saisie = sc.nextLine(); // peut recevoir q ou un nombre
-        
-        miles = kilometre*1/ 1.609;
-        System.out.printf("%.2f kilomètre = %.2f Miles", kilometre ,miles);
-         
-                
-        if(saisie == "q") 
+            
+            
+          
+            if(saisie == "q") 
         {
+            System.out.println("Quitte le programme");
             System.exit(0); // si on rentre q le programme s'arrête
         }
+
+            chaine = saisie.split(" ");
+
+            int valeur = Integer.parseInt(chaine[0]);
+            System.out.println(valeur + "fin");
+
+        }
+        while (kilometre < 0.01 || kilometre > 1000000); // fera le calcul si les kilometres et entre les deux proposition
+
        
-          
-        
-        
+        miles = kilometre*1/ 1.609;
+        System.out.printf("%.2f kilomètre = %.2f Miles", kilometre ,miles);
+     
 
         sc.close(); // obligatoire (libère les ressource)
 
