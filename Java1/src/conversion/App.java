0dc35c5b78b2d2c94b769054abcdcf861b41237f
kevin <kbourguignon@hotmail.com>
2021-06-15 16:59:22 +0200

    Update App.java

--- Java1/src/conversion/App.java
+++ Java1/src/conversion/App.java
@@ -14,34 +14,49 @@ public class App
         double kilometre;
         Scanner sc;
         String saisie;
+        String[] chaine;
         
         sc = new Scanner(System.in);
        
-       
+        saisie = sc.nextLine();
 
         // vos Algoritme
        
          do{
              System.out.println("Entrer les kilomètre ou appuyer sur q"); // mettre un nombre pour continuer ou une lettre pour arréter
+             
+             
+             
+
+             if(saisie == "q")
+             {
+                 System.out.println("quitte le programme");
+                 System.exit(0);
+           }
+           else 
+          {
+              Int q = Integer.parseInt(saisie);
+          }
+
              kilometre = sc.nextDouble();
              double miles = kilometre * 1.609;
              
              System.out.printf("%.2f miles", miles);
              
-             saisie = sc.nextLine();
+              
+
+             
+    
+            }while(kilometre <= 0.01 || kilometre >= 1000000);
+            
+            
 
             
-         }while(kilometre <= 0.01 || kilometre >= 1000000);
-         
-         if(saisie == "q") 
-        {   
-            
-            System.out.println("Quitte le programme");
-            
-        }
-         
+
+             sc.close(); // obligatoire (libère les ressource)
+
         
-        sc.close(); // obligatoire (libère les ressource)
+        
 
     }
 
