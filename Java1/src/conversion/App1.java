e95a7d61adb767e6d3bd436b7b5218c5f1440c2b
kevin <kbourguignon@hotmail.com>
2021-06-16 14:31:11 +0200

    Update App1.java

--- Java1/src/conversion/App1.java
+++ Java1/src/conversion/App1.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class App1 
 {
+    public static char getCharFromString(String phrase, int index)
+    {
+        return phrase.charAt(index);
+    }
+
     // code du composant 
     public static void main(String[] args) {
 
@@ -13,21 +18,31 @@ public class App1
         String saisie;
         String [] chaine;
 
-       
-        
-
+   
         //Vos algorithme
-        
-        
-        
+ 
         do
         {
             System.out.println("Entrer un nombre suivi de miles ou kilometre");
             sc = new Scanner(System.in);
             saisie = sc.nextLine(); // peut recevoir q ou un nombre 
 
-            System.out.println("Veuillez rentrer le kilométrage : ");
-            kilometre = sc.nextDouble();
+            if(saisie.equals("q"))
+             {
+                 System.out.println("quitter le programme");
+                 System.exit(0);
+           }
+
+           chaine = saisie.split(" ");
+           kilometre = Double.parseDouble(chaine[0]);
+
+           if(kilometre < 0.01 || kilometre > 1000000)
+           {
+               System.out.println("veuillez choisir un notre nombre compris entre 0.01 ou 1 000 000");
+               continue; // sa te permet de rerentrer la saisie 
+
+            
+            
         
         
         }
@@ -36,15 +51,7 @@ public class App1
         miles = kilometre*1.609;
         kilometre = miles/1.609;
 
-        chaine = saisie.split(" ");
-
-         
-
-        
-
-        
-
-        
+        System.out.printf("%.2f kilometre = %.3f miles", kilometre, miles);
 
 
         sc.close();
