4bdf45fc5aceccbff8b5d6c342f1b1de70e5f026
kevin <kbourguignon@hotmail.com>
2021-06-30 16:29:41 +0200

    creation Animal & App

---
+++ Java1/objet/introduction/Animal.java
@@ -0,0 +1,34 @@
+package introduction;
+
+
+public class Animal 
+{
+    private String specie;
+
+    public Animal(String _specie)
+    {
+        System.out.println("constructeur Animal");
+
+        this.specie = _specie;
+    }
+
+    public String getSpecie()
+    {
+        return this.specie;
+    }
+
+    public void feed()
+    {
+        System.out.println(this.specie + "  mange ! ");
+    }
+
+    public void move()
+    {
+        System.out.println(this.specie + " se déplace  ");
+    }
+
+
+
+
+    
+}
---
+++ Java1/objet/introduction/App.java
@@ -0,0 +1,17 @@
+package introduction;
+
+public class App 
+{
+
+    public static void main(String[] args) 
+    {
+        Animal animal = new Animal("Animal");
+        
+        System.out.println(animal.getSpecie());
+
+
+        
+    }
+
+    
+}
