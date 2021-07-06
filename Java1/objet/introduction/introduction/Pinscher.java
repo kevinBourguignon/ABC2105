41961e0e063c700e4a73ee00bef78f32bdf80e1f
kevin55100 <85477585+kevin55100@users.noreply.github.com>
2021-07-06 16:39:27 +0200

    update

--- Java1/objet/introduction/introduction/Pinscher.java
+++ Java1/objet/introduction/introduction/Pinscher.java
@@ -1,5 +1,7 @@
 package introduction;
 
+import java.util.TimerTask;
+import java.util.Timer;
 
 public class Pinscher extends Dog
 {
@@ -19,9 +21,32 @@ public class Pinscher extends Dog
         super.feed();
     }
 
-    public void barks()
+    public class Test extends Pinscher{
+    
+        public static Test obj;
+
+    public static void main(String[] args) throws InterruptedException
     {
-        
+        obj = new Test();
+
+        // création de l'instance du timer class
+        Timer timer = new Timer();
+
+        // création de l'instance de task programmé
+        TimerTask task = new Helper();
+
+        timer.schedule(task, 500, 9000);
+
+   
+        synchronized(obj)
+        {
+            obj.wait();
+        }
+
+        timer.cancel();
+
+    }
+    
     }
 
     
