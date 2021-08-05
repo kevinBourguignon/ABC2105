37db3ef6d644dd65bfff55fa127fb930ebaccf1e
kevin <kbourguignon@hotmail.com>
2021-08-05 10:41:27 +0200

    UPDATE CARDGAME

--- Merise/cardgame/cardgame.sql
+++ Merise/cardgame/cardgame.sql
@@ -15,5 +15,20 @@ CREATE TABLE IF NOT EXISTS players
 CREATE TABLE IF NOT EXISTS cards
 (
 	card_name VARCHAR(20) PRIMARY KEY,
-    card_power INT,
+    card_power INT NOT NULL,
+    card_armor INT NOT NULL,
+    card_spell VARCHAR(255) NOT NULL
+);
 
+CREATE TABLE IF NOT EXISTS cardtypes
+(
+	cardtype_id INT PRIMARY KEY,
+    cardtype_calssname VARCHAR(50) NULL NOT NULL,
+    cardtype_description CHAR(100) 
+);    
+
+CREATE TABLE IF NOT EXISTS energies
+(
+	energy_name VARCHAR(15),
+    energy_description CHAR(100)
+);    
