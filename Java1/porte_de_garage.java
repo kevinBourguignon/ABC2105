13f74ec6d49519ea5d6f1a4f862f807efbb993c7
kevin55100 <85477585+kevin55100@users.noreply.github.com>
2021-09-08 09:31:34 +0200

    Create porte_de_garage.java

---
+++ Java1/porte_de_garage.java
@@ -0,0 +1,47 @@
+package portedegarage;
+
+
+public class porte_de_garage {
+
+	private double ouvertureEnPourcentage;
+	private boolean verrouille;
+
+
+	public PorteDeGarage(){
+
+	}
+
+
+	public boolean fermer(double pourcentageFermetureSupplementaire){
+		
+	}
+
+	public boolean ouvrir(double pourcentageouverturesupplementaire){
+		
+	}
+
+	public boolean fermerTotalement(){
+		
+	}
+
+	public boolean ouvrirTotalement(){
+		
+	}
+
+	public double getOuvertureEnPourcentage(){
+		
+	}
+
+	public boolean verrouiller(){
+		
+	}
+
+	public boolean deverouiller(){
+		
+	}
+
+	public boolean getVerouille(){
+		
+	}
+
+}
