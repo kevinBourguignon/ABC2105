90f0ff5189c8bf879d52e9f15cd9ddf035edb993
kevin55100 <85477585+kevin55100@users.noreply.github.com>
2021-09-08 11:49:50 +0200

    Update portedegarage.java

--- Java1/PorteDeGarage/portedegarage.java
+++ Java1/PorteDeGarage/portedegarage.java
@@ -3,49 +3,46 @@ package portedegarage;
 
 public class PorteDeGarage {
 
-		
-	
-
 	private double ouvertureEnPourcentage;
 	private boolean verrouille;
-	
 
+	
+	
 	public PorteDeGarage(){
 
 	}
 
 
 	public boolean fermer(double pourcentageFermetureSupplementaire){
-		
+		return false;
 	}
 
 	public boolean ouvrir(double pourcentageouverturesupplementaire){
-		
+		return false;
 	}
 
 	public boolean fermerTotalement(){
-		
+		return false;
 	}
 
 	public boolean ouvrirTotalement(){
-		
+		return false;
 	}
 
 	public double getOuvertureEnPourcentage(){
-		
+		return 0;
 	}
 
 	public boolean verrouiller(){
-		
+		return false;
 	}
 
 	public boolean deverouiller(){
-		
+		return false;
 	}
 
 	public boolean getVerouille(){
-		
+		return false;
 	}
-	
-	
+
 }
