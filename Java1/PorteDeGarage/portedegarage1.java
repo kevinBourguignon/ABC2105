b003c96080552341c7e9c534d1927b76f7a98006
kevin55100 <85477585+kevin55100@users.noreply.github.com>
2021-09-09 08:24:56 +0200

    Create portedegarage1.java

---
+++ Java1/PorteDeGarage/portedegarage1.java
@@ -0,0 +1,74 @@
+package portedegarage;
+
+public class portedegarage1
+{
+
+	private double ouvertureEnPourcentage;
+	private boolean verrouiller;
+	
+
+	
+	public portedegarage1(){
+		verrouiller = true;
+		ouvertureEnPourcentage = 0;
+
+	}
+
+
+	public boolean fermer(double pourcentageFermetureSupplementaire){
+		return false;
+	}
+
+	public boolean ouvrir(double pourcentageouverturesupplementaire){
+		return false;
+	}
+
+	public boolean fermerTotalement(){
+		return false;
+	}
+
+	public boolean ouvrirTotalement(){
+		return false;
+	}
+
+	public double getOuvertureEnPourcentage(){
+		return 0;
+	}
+
+	public boolean verrouiller(){
+		if(verrouiller = false && ouvertureEnPourcentage == 0)
+		{
+			System.out.println("la porte se vérrouille");
+			verrouiller = false;
+			return false;
+		}
+		else{
+			System.out.println("la porte est  vérrouille");
+			return false;
+		}
+		
+		
+
+	}
+
+	public boolean deverouiller(){
+		
+		if(verrouiller = true && ouvertureEnPourcentage == 0)
+		{
+			System.out.println("la porte se déverouille");
+			verrouiller = true;
+			return true;
+		}
+		else{
+			System.out.println("la porte est déverrouillée");
+			return true;
+		}
+		
+
+	}
+
+	public boolean getVerouille(){
+		return false;
+	}
+
+}
