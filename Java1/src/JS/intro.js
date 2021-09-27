32a65cd66b53276f5d280fa7582c917586d8a7da
kevin <kbourguignon@hotmail.com>
2021-09-27 12:53:50 +0200

    Update intro.js

--- Java1/src/JS/intro.js
+++ Java1/src/JS/intro.js
@@ -1,3 +1,26 @@
+let a = 2;
+let b = 4;
+
+console.log(".....Inversion avant.....")
+console.log("A : " +a)
+console.log("B : " +b)
+
+tmp = a;
+a=b;
+b=tmp;
+
+console.log(".....Inversion après.....")
+console.log("A : " +a)
+console.log("B : " +b)
+
+
+let nom = "Mamadou"
+let prenom = "Gayamar"
+let age = "25 ans"
+
+console.log("Nom : " +nom)
+console.log("Prénom : " +prenom)
+console.log("Age : " +age)
 
 
 
