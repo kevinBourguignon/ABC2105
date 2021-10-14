c8d641d8ffd316f9d4138b150738b472c997696f
kevin <kbourguignon@hotmail.com>
2021-10-14 08:55:13 +0200

    update

--- Java1/src/Counter/counter.js
+++ Java1/src/Counter/counter.js
@@ -1,25 +1,46 @@
 
 
-//Vous n'êtes pas obligés de comprendre la class compteur
- class Compteur {
-    constructor(début, delaiEnMilisecondes) {
+//Vous n'êtes pas obligés de comprendre toute la classe compteur
+class Compteur {
+    constructor(début, delaiEnMilisecondes, elementHTML) {
         this.compte = début;
-        this.spanElement = document.createElement("span");
+        this.elementHTML = elementHTML;
 
         setInterval(() => {
             this.compte++;
-            this.spanElement.textContent = this.compte;
+            this.elementHTML.textContent = this.compte;
+            let a = document.querySelectorAll("div");
+            if(this.compte % 10 == 0){
+                for(let div of a ){
+                    let red = Math.floor(Math.random()*200);
+                    let blue = Math.floor(Math.random()*200);
+                    let green = Math.floor(Math.random()*200);
+                    div.style.backgroundColor = "rgba("+red+ ", "+blue+ "," +green+ ", 0.5)";
+                    console.log(div);
+
+                }
+            }
         }, delaiEnMilisecondes);
-      
     }
 }
 
 
 
+let div = document.getElementsByClassName("compteur-secondes")[0];
+let p = div.children[0];
+let span = p.children[0];
 
-let span = document.querySelector("body div");
+let empty = document.getElementsByClassName("empty")[0];
 
-let compteSeconde = new Compteur(0, 1000);
+/*Un compteur prend 3 paramètre d'entrée : 
+début : nombre auquel le compteur commence
+delaiEnMilisecondes : l'intervalle de temps entre chaque incrémentation du compteur
+elementHTML : élément HTML dans lequel le compteur est affiché
+ */
+let compteSeconde = new Compteur(0, 1000, span);
+let compteMinute = new Compteur(0, 60000, empty);
 
-span.parentElement.replaceChild(compteSeconde.spanElement, span);
-
+ let body = document.querySelector("body div");
+  body.style.backgroundColor = "green";
+//  let secondes = new body(0, 2000, empty)
+console.log(compteSeconde.compte);
