c068487a20ce447bc0e549fe5de9437df27f41cb
kevin <kbourguignon@hotmail.com>
2021-10-14 12:51:59 +0200

    Update counter.js

--- Java1/src/Counter/counter.js
+++ Java1/src/Counter/counter.js
@@ -5,35 +5,26 @@ class Compteur {
     constructor(début, delaiEnMilisecondes, elementHTML) {
         this.compte = début;
         this.elementHTML = elementHTML;
-        this.stop = false;
 
-       
         setInterval(() => {
             this.compte++;
             this.elementHTML.textContent = this.compte;
             let a = document.querySelectorAll("div");
             if(this.compte % 10 == 0){
-               if(stop == false){
-                   for(let div of a ){
+                for(let div of a ){
                     let red = Math.floor(Math.random()*200);
                     let blue = Math.floor(Math.random()*200);
                     let green = Math.floor(Math.random()*200);
                     div.style.backgroundColor = "rgba("+red+ ", "+blue+ "," +green+ ", 0.5)";
                     console.log(div);
-               } 
-                
 
                 }
             }
         }, delaiEnMilisecondes);
-    } 
-    stopcolor(){
-            this.stop = true;
-        }
+    }
 }
 
-let stop = document.getElementsByClassName("stop")[0];
-
+let stop = document.getElementsByClassName("stop");
 
 let div = document.getElementsByClassName("compteur-secondes")[0];
 let p = div.children[0];
@@ -53,4 +44,3 @@ let compteMinute = new Compteur(0, 60000, empty);
   body.style.backgroundColor = "green";
 //  let secondes = new body(0, 2000, empty)
 console.log(compteSeconde.compte);
-
